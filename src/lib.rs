fb889239a00713b258479fc0691d10f01c484804
oddg <odegevigney@gmail.com>
2019-06-27 10:05:33 -0700

    Implement iterator to enumerate permutations

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod enumerator;
 mod equivalence;
+mod permutation;
 
 pub use enumerator::SubsetEnumerator;
 pub use equivalence::EquivalenceClass;
+pub use permutation::SwapIterator;
--- src/permutation.rs
+++ src/permutation.rs
@@ -1,18 +1,123 @@
 /// https://en.wikipedia.org/wiki/Heap%27s_algorithm
-struct Permutation {
-    permutation: Vec<usize>,
-    // length of the fixed prefix
-    fixed: usize,
+pub struct SwapIterator {
+    n: usize,
+    c: Vec<usize>,
+    i: usize,
 }
 
-impl Iterator for Permutation {
-    type Item = Vec<usize>;
+impl SwapIterator {
+    pub fn new(n: usize) -> Self {
+        SwapIterator {
+            n,
+            c: vec![0; n],
+            i: 0,
+        }
+    }
+}
+
+impl Iterator for SwapIterator {
+    type Item = (usize, usize);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if fixed == (permutation.len() - 1) {
-            self.fixed -= 1;
-            return Some(self.permutation.clone());
+        if self.i < self.n {
+            if self.c[self.i] < self.i {
+                let item = if (self.i % 2) == 0 {
+                    (0, self.i)
+                } else {
+                    (self.c[self.i], self.i)
+                };
+                self.c[self.i] += 1;
+                self.i = 0;
+                Some(item)
+            } else {
+                self.c[self.i] = 0;
+                self.i += 1;
+                self.next()
+            }
+        } else {
+            None
         }
-        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cmp::Ordering;
+
+    fn factorial(n: usize) -> usize {
+        let mut f = n;
+        for i in 2..n {
+            f *= i;
+        }
+        f
+    }
+
+    // Check the following properties:
+    // - n! - 1 swaps are generated
+    // - applying those swaps yields a sequence of uniq permutations
+    fn test_helper(n: usize) {
+        let mut v = (0..n).collect::<Vec<usize>>();
+        let mut permutations = Vec::new();
+        permutations.push(v.clone());
+        for (i, j) in SwapIterator::new(n) {
+            let t = v[i];
+            v[i] = v[j];
+            v[j] = t;
+            permutations.push(v.clone());
+        }
+
+        assert_eq!(permutations.len(), factorial(n));
+        permutations.sort_by(|x, y| {
+            for (a, b) in x.iter().zip(y.iter()) {
+                if a < b {
+                    return Ordering::Less;
+                } else if a > b {
+                    return Ordering::Greater;
+                }
+            }
+            assert!(false, "two equal permutations");
+            panic!()
+        });
+    }
+
+    #[test]
+    fn check_length_and_uniqueness() {
+        test_helper(4);
+        test_helper(5);
+        test_helper(6);
+        test_helper(7);
+        test_helper(8);
+    }
+
+    #[test]
+    fn permutation_4() {
+        let expected = vec![
+            (0, 1),
+            (0, 2),
+            (0, 1),
+            (0, 2),
+            (0, 1),
+            (0, 3),
+            (0, 1),
+            (0, 2),
+            (0, 1),
+            (0, 2),
+            (0, 1),
+            (1, 3),
+            (0, 1),
+            (0, 2),
+            (0, 1),
+            (0, 2),
+            (0, 1),
+            (2, 3),
+            (0, 1),
+            (0, 2),
+            (0, 1),
+            (0, 2),
+            (0, 1),
+        ];
+        let generated = SwapIterator::new(4).collect::<Vec<(usize, usize)>>();
+        assert_eq!(expected, generated);
     }
 }
